1c88ac2ee4e626fef3075fcbe81d63906fb693e2
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2022-09-09 02:01:43 +0100

    New Changes

--- backend/settings.py
+++ backend/settings.py
@@ -3,8 +3,8 @@ import os
 
 load_dotenv()
 
-DATABASE_NAME = os.environ.get('trivia')
-DATABASE_NAME_2 = os.environ.get('trivia')
+DATABASE_NAME = os.environ.get('postgres')
+DATABASE_NAME_2 = os.environ.get('postgres')
 DATABASE_PORT = os.environ.get('5432')
 DATABASE_OWNER = os.environ.get('postgres')
 DATABASE_PASSWORD = os.environ.get(' ')
